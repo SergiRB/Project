703b849ee4f55b6e6b88b692048b9d0f1f77d3de
Sergi Roca-Bonet <sergi.rb94@gmail.com>
2017-02-22 14:49:13 +0100

    second file

--- test.f90
+++ test.f90
@@ -1,7 +1,11 @@
 program test_git
 
 	implicit none
+	character(len=29)  ::  name
 
-	print *, "Hello World"
+	!print *, "Hello World"
+
+	read *, name
+	print *, "Hello, ", name
 
 end program test_git
